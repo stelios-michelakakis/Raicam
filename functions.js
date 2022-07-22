16eaf0ec699f960df07667a1b1b16300502ce06f
Stylianos Michelakakis <csd3524@csd.uoc.gr>
2022-07-22 13:40:38 +0300

    Bugs fixed, added 2 more consortium members

--- functions.js
+++ functions.js
@@ -29,3 +29,18 @@ home.addEventListener('click', ()=>{
   });
 })
 }
+
+const consortium_btn = document.getElementById("consortium_btn");
+
+consortium_btn.addEventListener('click', ()=>{
+  console.log("hello");
+  var element = document.getElementById('consortium');
+  var headerOffset = 45;
+  var elementPosition = element.getBoundingClientRect().top;
+  var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+  window.scrollTo({
+       top: offsetPosition,
+       behavior: "smooth"
+  });
+})
