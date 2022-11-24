541055dcb8d90c9d63ffb3b75c9c4ecebff4c166
Stelios Michelakakis <57458829+stelios-michelakakis@users.noreply.github.com>
2022-11-24 18:24:53 +0200

    Added additional functionality and security walls on the form

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 let ids = document.getElementsByClassName("position_component");
 $(document).ready(function () {
+  
   for (i = 0; i < 10; i++) {
     if (window.location.href.indexOf(ids[i].getAttribute("id")) != -1) {
       var elementPosition = ids[i].getBoundingClientRect().top;
@@ -16,6 +17,36 @@ $(document).ready(function () {
     }
   }
 });
+
+
+const array_institutes = ["Technical University of Munich", "Technical University of Munich", "Universidad De Seville", "Tampere University", "Karlsruhe Institute of Technology", "University of Turku", "Istituto Italiano Di Technologia", "École nationale supérieure de techniques avancées Paris", "The University of West of England", "The University of Manchester"];
+var selection1 = document.getElementById("institute1");
+var selection2 = document.getElementById("institute2");
+var selection3 = document.getElementById("institute3");
+
+var select = document.getElementById("example-select");
+
+
+selection1.addEventListener('change', function() {
+if(selection1.value==selection2.value || selection1.value==selection3.value){
+  selection1.value=selection1.options[1];
+  alert("You can't enter the same institute twice.");
+}
+})
+
+selection2.addEventListener('change', function() {
+if(selection2.value==selection1.value || selection2.value==selection3.value){
+  selection2.value=selection2.options[1];
+  alert("You can't enter the same institute twice.");
+}
+})
+
+selection3.addEventListener('change', function() {
+if(selection3.value==selection1.value || selection2.value==selection3.value){
+  selection3.value=selection3.options[1];
+  alert("You can't enter the same institute twice.");
+}
+})
 
 const selected_button = Array.from(document.getElementsByClassName("map_button"))
 const info = document.getElementById("info_container");
@@ -88,3 +119,16 @@ consortium_btn.addEventListener('click', () => {
     behavior: "smooth"
   });
 })
+
+
+
+const submit_btn = document.getElementById("submit");
+
+submit_btn.addEventListener('click', () => {
+
+  if (selection1 == selection2 || selection2 == selection3 || selection3 == selection1) {
+    alert("Institutes of preference must be unique in each selection!");
+    console.log("Same");
+  }
+}
+)
